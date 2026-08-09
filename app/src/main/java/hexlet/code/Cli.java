6249d928e158491bdfc410c14ac7e3c561f0ea9d
Endurance9102 <snm21071999@gmail.com>
2026-08-10 00:45:41 +0300

    greetin

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void Example () {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        System.out.println("Hello, " + getName() + "!");
+    }
+    public static String getName () {
+        Scanner user = new Scanner(System.in);
+        return user.nextLine();
+    }
+}
