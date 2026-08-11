175831a8f843b45987cf5415a47c9ffbe04557eb
Endurance9102 <snm21071999@gmail.com>
2026-08-11 23:03:35 +0300

    Choice game

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -12,3 +12,29 @@ public class Cli {
         return user.nextLine();
     }
 }
+
+
+
+class ChooseGame {
+    public static void choose() {
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        int resultUserChoice = getChoose();
+        System.out.println("Your choice: " + resultUserChoice);
+        if (resultUserChoice == 1) {
+            Cli.example();
+        } else if (resultUserChoice == 0) {
+            System.exit(0);
+        } else {
+            System.out.println("invalid value");
+            choose();
+        }
+
+
+    }
+    public static int getChoose() {
+        Scanner number = new Scanner(System.in);
+        return number.nextInt();
+    }
+}
